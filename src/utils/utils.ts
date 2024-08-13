d80cca4f921ce999fa3d2f928b952a316cfdb5e2
Japheth3377 <japheth.jonathan@synergyy.io>
2024-08-13 12:44:18 +0100

    endpoint fix

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,16 +1,16 @@
 // import { userAdmin } from '@/zustand/state';
 import axios from 'axios';
 // import { useEffect } from 'react';
-// import * as dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 
-// dotenv.config();
+dotenv.config();
 
-const baseURL =
-  process.env.NODE_ENV === 'production'
-    ? `${process.env.Base}`
-    : 'http://localhost:3001';
+// const baseURL =
+//   process.env.NODE_ENV === 'production'
+//     ? `${process.env.Base}`
+//     : 'http://localhost:3001';
 
-// const baseURL = "https://synergy-api-aoea.onrender.com/"
+const baseURL = "https://synergy-api-aoea.onrender.com/"
 
 const axiosInstance = axios.create({
   baseURL,
