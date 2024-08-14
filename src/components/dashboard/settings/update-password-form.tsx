043b3b482618a087d5fc0164625e9cdcdca63ced
Japheth3377 <japheth.jonathan@synergyy.io>
2024-08-14 09:34:07 +0100

    update2

--- src/components/dashboard/settings/update-password-form.tsx
+++ src/components/dashboard/settings/update-password-form.tsx
@@ -1,6 +1,6 @@
 'use client';
-
 import * as React from 'react';
+import { ChangeEvent } from 'react';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -11,8 +11,25 @@ import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import Stack from '@mui/material/Stack';
+import axiosInstance from '@/utils/utils';
 
 export function UpdatePasswordForm(): React.JSX.Element {
+  const [username, setusername] = React.useState("")
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+      setusername(event.target.value);
+  };
+
+
+  const handleSubmit = async () => {
+    const body = {
+      username,
+      user_id: null,
+    };
+    const response = await axiosInstance.post('/admins/reserved-usernames', body);
+    return response
+    // console.log('Response:', response.data);
+  };
   return (
     <form
       onSubmit={(event) => {
@@ -26,13 +43,20 @@ export function UpdatePasswordForm(): React.JSX.Element {
           <Stack spacing={3} sx={{ maxWidth: 'sm' }}>
             <FormControl fullWidth>
               <InputLabel>Add restricted username</InputLabel>
-              <OutlinedInput label="add a restricted username" name="username" type="text" />
+              <OutlinedInput
+                value={username}
+                onChange={handleChange}
+                label="Add a restricted username"
+                name="username"
+                type="text"
+                fullWidth
+              />
             </FormControl>
           </Stack>
         </CardContent>
         <Divider />
         <CardActions sx={{ justifyContent: 'flex-end' }}>
-          <Button variant="contained">Update</Button>
+          <Button variant="contained" onClick={handleSubmit}>Update</Button>
         </CardActions>
       </Card>
     </form>
